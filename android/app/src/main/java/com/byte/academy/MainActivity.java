b8f6408ec837f4166b185eac715cffc918c4a4c6
Skoru <skoru1972@gmail.com>
2026-03-17 01:05:33 +0100

    Enable zoom and move module toasts

--- android/app/src/main/java/com/byte/academy/MainActivity.java
+++ android/app/src/main/java/com/byte/academy/MainActivity.java
@@ -1,5 +1,24 @@
 package com.byteacademy.app;
 
+import android.os.Bundle;
+import android.webkit.WebSettings;
+
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		if (bridge == null || bridge.getWebView() == null) {
+			return;
+		}
+
+		WebSettings webSettings = bridge.getWebView().getSettings();
+		webSettings.setSupportZoom(true);
+		webSettings.setBuiltInZoomControls(true);
+		webSettings.setDisplayZoomControls(false);
+		webSettings.setUseWideViewPort(true);
+		webSettings.setLoadWithOverviewMode(true);
+	}
+}
